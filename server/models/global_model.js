c3685bdd09c9e76f9fb882f1216f4c78ab1c4838
Eli <eli.delventhal@gmail.com>
2019-08-26 17:23:19 -0700

    Do mentions, add emoji

--- server/models/global_model.js
+++ server/models/global_model.js
@@ -23,24 +23,24 @@ var GlobalModel = module.exports =
                     return;
                 }
                 
-                const usernames = [];
+                const userIds = [];
                 let lowestCoffeeCount = -1;
                 Object.keys( userData.members ).forEach( function( userKey )
                 {
                     const user = userData.members[userKey];
-                    if ( !user.deleted && !user.is_bot && user.name !== "slackbot" ) //why isn't slackbot a bot?
+                    if ( !user.deleted && !user.is_bot && !user.is_app_user && user.name !== "slackbot" ) //why isn't slackbot a bot?
                     {
-                        usernames.push( user.name );
+                        userIds.push( user.id );
                         
                         //store the least number of times a user has had coffee
-                        if ( lowestCoffeeCount < 0 || ( pastData.timesPaired[user.name] && pastData.timesPaired[user.name] < lowestCoffeeCount ) )
+                        if ( lowestCoffeeCount < 0 || ( pastData.timesPaired[user.id] && pastData.timesPaired[user.id] < lowestCoffeeCount ) )
                         {
-                            lowestCoffeeCount = pastData.timesPaired[user.name] || 0;
+                            lowestCoffeeCount = pastData.timesPaired[user.id] || 0;
                         }
                     }
                 });
                 
-                if ( usernames.length < 2 )
+                if ( userIds.length < 2 )
                 {
                     cb( "There needs to be at least 2 users!" );
                     return;
@@ -49,55 +49,55 @@ var GlobalModel = module.exports =
                 //find the users that have had coffee the least number of times so we can choose one of them
                 const lowestCountUsers = [];
                 
-                usernames.forEach( function( username )
+                userIds.forEach( function( userId )
                 {
-                    if ( ( pastData.timesPaired[username] || 0 ) <= lowestCoffeeCount )
+                    if ( ( pastData.timesPaired[userId] || 0 ) <= lowestCoffeeCount )
                     {
-                        lowestCountUsers.push( username );
+                        lowestCountUsers.push( userId );
                     }
                 });
                 
                 //now that we know which users have had coffee the least times, choose a user to have coffee
                 const primaryUserIndex = Math.floor( Math.random() * lowestCountUsers.length );
-                const primaryUsername = lowestCountUsers[primaryUserIndex];
+                const primaryUserId = lowestCountUsers[primaryUserIndex];
                 lowestCountUsers.splice( primaryUserIndex, 1 );
                 
                 //and then choose a partner for that user by finding the lowest pair count among them and other users
-                const primaryPairs = pastData.pairs[primaryUsername] || {};
+                const primaryPairs = pastData.pairs[primaryUserId] || {};
                 let lowestPartnerCount = -1;
-                usernames.forEach( function( username )
+                userIds.forEach( function( userId )
                 {
                     //store the least number of times a partner has had coffee with this user
-                    if ( username !== primaryUsername && ( lowestPartnerCount < 0 || ( ( primaryPairs[username] || 0 ) < lowestCoffeeCount ) ) )
+                    if ( userId !== primaryUserId && ( lowestPartnerCount < 0 || ( ( primaryPairs[userId] || 0 ) < lowestCoffeeCount ) ) )
                     {
-                        lowestPartnerCount = primaryPairs[username] || 0;
+                        lowestPartnerCount = primaryPairs[userId] || 0;
                     }
                 });
                 
                 const validPartners = [];
-                usernames.forEach( function( username )
+                userIds.forEach( function( userId )
                 {
-                    const pairCount = ( primaryPairs[username] || 0 );
-                    if ( username !== primaryUsername && pairCount <= lowestPartnerCount )
+                    const pairCount = ( primaryPairs[userId] || 0 );
+                    if ( userId !== primaryUserId && pairCount <= lowestPartnerCount )
                     {
-                        validPartners.push( username );
+                        validPartners.push( userId );
                     }
                 });
                 
                 //finally, choose their partner
-                const partnerUsername = validPartners[Math.floor( Math.random() * validPartners.length )];
-                const partnerPairs = pastData.pairs[partnerUsername] || {};
+                const partnerUserId = validPartners[Math.floor( Math.random() * validPartners.length )];
+                const partnerPairs = pastData.pairs[partnerUserId] || {};
                 
                 //have the Slackbot post the pairs
-                slack.post( "It's coffee time! Today it's @" + primaryUsername + " and @" + partnerUsername + "!", function( success, error )
+                slack.post( ":coffee:It's coffee time!:coffee2:\nThis week it's <@" + primaryUserId + "> and <@" + partnerUserId + ">!\n\nGo see Charley to get your gift card, then go get coffee and take a selfie! :parrot-coffee:", function( success, error )
                 {
                     if ( success )
                     {
                         //update the data to be written to the DB
-                        pastData.timesPaired[primaryUsername] = ( pastData.timesPaired[primaryUsername] || 0 ) + 1;
-                        pastData.timesPaired[partnerUsername] = ( pastData.timesPaired[partnerUsername] || 0 ) + 1;
-                        pastData.pairs[primaryUsername] = ( primaryPairs[partnerUsername] || 0 ) + 1;
-                        pastData.pairs[partnerUsername] = ( partnerPairs[primaryUsername] || 0 ) + 1;
+                        pastData.timesPaired[primaryUserId] = ( pastData.timesPaired[primaryUserId] || 0 ) + 1;
+                        pastData.timesPaired[partnerUserId] = ( pastData.timesPaired[partnerUserId] || 0 ) + 1;
+                        pastData.pairs[primaryUserId] = ( primaryPairs[partnerUserId] || 0 ) + 1;
+                        pastData.pairs[partnerUserId] = ( partnerPairs[primaryUserId] || 0 ) + 1;
 
                         //write the result to the db
                         database.set( HISTORY_KEY, JSON.stringify( pastData ), function( dbSuccess )
